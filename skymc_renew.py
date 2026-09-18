2f9143fecb25742cb4ddbdf355f806f1beba2956
larkspur8506 <larkspur85@gmail.com>
2026-09-19 01:43:48 +0800

    Update skymc_renew.py

--- skymc_renew.py
+++ skymc_renew.py
@@ -25,9 +25,9 @@ PASSWORD = os.environ.get("SKYMC_PASSWORD") or os.environ.get("PASSWORD") or ""
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN") or ""
 TG_CHAT_ID = os.environ.get("TG_CHAT_ID") or ""
 
-SERVER_URL = os.environ.get("SERVER_URL") or "https://skymc.org/en/server/TuUzR_dWxO2P"
+SERVER_URL = os.environ.get("SERVER_URL") or "https://skymc.org/en/server/xWFsEb7CjA2n"
 LOGIN_URL = "https://skymc.org/en/login"
-SERVER_ID = "TuUzR_dWxO2P"
+SERVER_ID = "xWFsEb7CjA2n"
 
 NODE_LINK = (os.environ.get("NODE_LINK") or "").strip()
 IS_PROXY = os.environ.get("IS_PROXY", "false").lower() == "true"
